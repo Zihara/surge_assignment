21d189b949ae73a903bd9c048d72dac7c7cd815c
ZIHARA M.I.F <86686054+Zihara@users.noreply.github.com>
2022-07-25 00:15:09 +0530

    Server initiated

---
+++ Backend/index.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.SOCKET_PORT || 4040;
+const MONGODB_URL = process.env.MONGODB_URL;
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+const Connect_Database = () => {
+    mongoose.connect(MONGODB_URL).then(() => {
+        console.log(`Successfully MongoDB Connected!`)
+    }).catch((error) => {
+        console.log(`${error}`)
+    })
+}
+
+// Server with mongodb connection
+app.listen(PORT, () => {
+    console.log(`Server initiated and running on port: ${PORT}`);
+    Connect_Database();
+});
