82389fe27328e2e78e6390c02335a60b88b6d036
ZIHARA M.I.F <86686054+Zihara@users.noreply.github.com>
2022-07-27 06:47:45 +0530

    toast message modified

--- frontend/src/utils/Message.jsx
+++ frontend/src/utils/Message.jsx
@@ -1,15 +1,15 @@
 import {toast} from "react-toastify";
 
-const Message = (message,type,timeout) => {
+const Message = (message,type,timeout,position) => {
     switch (type){
         case 'info':
-            return toast.info(message,{theme: "light",autoClose:timeout})
+            return toast.info(message,{theme: "light",autoClose:timeout,position:position})
         case 'success':
-            return toast.success(message,{theme: "light",autoClose:timeout})
+            return toast.success(message,{theme: "light",autoClose:timeout,position:position})
         case 'warning':
-            return toast.warning(message,{theme: "light",autoClose:timeout})
+            return toast.warning(message,{theme: "light",autoClose:timeout,position:position})
         case 'error':
-            return toast.error(message,{theme: "light",autoClose:timeout})
+            return toast.error(message,{theme: "light",autoClose:timeout,position:position})
         default:
             return toast(message,{autoClose:timeout})
     }
